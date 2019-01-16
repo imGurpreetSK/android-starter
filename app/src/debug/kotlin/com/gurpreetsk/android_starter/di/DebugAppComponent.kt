84d200d68417b7f26f3aac69017140de365d32c1
Gurpreet Singh <gurpreetsk@hotmail.com>
2019-01-16 23:09:59 +0530

    AppComponents, update contract and modules.

--- app/src/debug/kotlin/com/gurpreetsk/android_starter/di/DebugAppComponent.kt
+++ app/src/debug/kotlin/com/gurpreetsk/android_starter/di/DebugAppComponent.kt
@@ -2,12 +2,16 @@ package com.gurpreetsk.android_starter.di
 
 import com.gurpreetsk.android_starter.di.modules.AppModule
 import com.gurpreetsk.android_starter.di.modules.DebugLogModule
+import com.gurpreetsk.android_starter.di.modules.NetworkModule
+import com.gurpreetsk.android_starter.di.modules.StorageModule
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
 @Component(modules = [
   AppModule::class,
-  DebugLogModule::class
+  DebugLogModule::class,
+  NetworkModule::class,
+  StorageModule::class
 ])
 interface DebugAppComponent : AppComponent
--- app/src/main/kotlin/com/gurpreetsk/android_starter/di/AppComponent.kt
+++ app/src/main/kotlin/com/gurpreetsk/android_starter/di/AppComponent.kt
@@ -4,6 +4,10 @@ import android.content.Context
 import com.gurpreetsk.android_starter.MainApplication
 import com.gurpreetsk.android_starter.di.modules.AppModule
 import com.gurpreetsk.android_starter.di.modules.LogModule
+import com.gurpreetsk.android_starter.di.modules.NetworkModule
+import com.gurpreetsk.android_starter.di.modules.StorageModule
+import com.gurpreetsk.android_starter.storage.db.AppDatabase
+import com.gurpreetsk.android_starter.storage.prefs.AppSettings
 import dagger.Component
 import timber.log.Timber
 import javax.inject.Singleton
@@ -11,9 +15,14 @@ import javax.inject.Singleton
 @Singleton
 @Component(modules = [
   AppModule::class,
-  LogModule::class
+  LogModule::class,
+  NetworkModule::class,
+  StorageModule::class
 ]) interface AppComponent {
   fun timberTree(): Timber.Tree
+
+  fun appDatabase(): AppDatabase
+  fun appSettings(): AppSettings
 
   companion object {
     fun obtain(context: Context): AppComponent =
