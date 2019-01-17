904d9a581490ee06449dcaf93f5444c4a686015f
Gurpreet Singh <gurpreetsk@hotmail.com>
2019-01-17 11:13:24 +0530

    CanaryTest, create unit test file.

---
+++ app/src/test/kotlin/com/gurpreetsk/android_starter/CanaryTest.kt
@@ -0,0 +1,11 @@
+package com.gurpreetsk.android_starter
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class CanaryTest {
+  @Test fun `This will fail if the test environment setup isn't correct`() {
+    assertThat(true)
+        .isTrue()
+  }
+}
